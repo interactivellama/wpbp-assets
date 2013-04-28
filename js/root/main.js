7309c882b7f0ea63f198c8e1d7053db407d803c4
stephen <stephen@welldonemarketing.com>
2013-04-28 00:30:01 -0400

    Editing readmes

--- js/root/main.js
+++ js/root/main.js
@@ -4,6 +4,7 @@
 	Import libraries/dependent scripts into one file and minify with CodeKit
 	Visit http://www.incident57.com/codekit for more information
 
+	Vendor
 	@codekit-prepend "wpbp-assets/js/vendor/smartresize.js";
 	@codekit-prepend "wpbp-assets/js/vendor/console-wrapper.js";
   @codekit-prepend 'wpbp-assets/js/vendor/bootstrap/js/bootstrap.js';
@@ -11,6 +12,8 @@
   @codekit-prepend 'wpbp-assets/js/vendor/formidable/formidable.js';	
   @codekit-prepend 'wpbp-assets/js/vendor/bxslider-4-master/jquery.bxslider.js';
 
+  Custom
+  @codekit-prepend 'wpbp-assets/js/formidable-forms.js';
 	@codekit-prepend "wpbp-assets/js/jquery.safetoggle.js";
 
 */
