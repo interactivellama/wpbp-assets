9edffbb9d4ad7413760269e4ab2f0f047608ffa2
stephen <stephen@welldonemarketing.com>
2013-07-15 10:10:21 -0400

    Can specify toggle on or off in safeToggle plugin

--- js/jquery.safetoggle.js
+++ js/jquery.safetoggle.js
@@ -1,20 +1,38 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - -
 	Safe Toggle Slide
-	show/hide menu (similar to bootstrap's NavBar JavaScript)
+	Show/Hide an element (similar to bootstrap's NavBar JavaScript)
 - - - - - - - - - - - - - - - - - - - - - - - - */
 
 (function($) {
 	"use strict";
 
-	$.fn.safeToggle = function() {
+	$.fn.safeToggle = function(forced) {
+	
+		var element = $(this);
+		
+		if (forced === 'show' && element.hasClass('collapsed')) {	
+			show(element);
+		}
+		else if(forced === 'hide' && !element.hasClass('collapsed')) {	
+			hide(element);	
+		}
+		else if(typeof forced == 'undefined' && !element.hasClass('collapsed') ) {
+			hide(element);
+		}
+		else if(typeof forced == 'undefined' && element.hasClass('collapsed') ) {
+			show(element);
 
-		if (!$(this).hasClass('collapsed')) {
-			$(this).slideUp('slow', function() {
-				$(this).addClass('collapsed').slideDown(0);
+		}
+		
+		function show(element) {
+			element.slideUp(0, function() {
+				element.removeClass('collapsed').slideDown('fast');
 			});
-		} else {
-			$(this).slideUp(0, function() {
-				$(this).removeClass('collapsed').slideDown('fast');
+		}
+
+		function hide(element) {
+			element.slideUp('slow', function() {
+				element.addClass('collapsed').slideDown(0);
 			});
 		}
 
